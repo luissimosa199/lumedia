d6b570c0966b9f5ac3329a82be33ab2474745ced
Luis Simosa <simosa37@gmail.com>
2023-02-27 10:42:20 -0300

    post prisma schema & postRouter created

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+import {
+    createTRPCRouter,
+    publicProcedure,
+    protectedProcedure,
+} from "~/server/api/trpc";
+
+export const postRouter = createTRPCRouter({
+
+    getAll: publicProcedure.query(({ ctx }) => {
+        return ctx.prisma.post.findMany()
+    }),
+
+    getOne: publicProcedure
+        .input(z.object({ id: z.string() }))
+        .query(({ ctx, input }) => {
+            return ctx.prisma.post.findUnique({ where: { id: input.id } })
+        }),
+
+    create: protectedProcedure
+        .input(z.object({
+            title: z.string(),
+            content: z.string(),
+            tags: z.string().array(),
+            authorName: z.string(),
+        }))
+        .mutation(({ ctx, input }) => {
+            return ctx.prisma.post.create({
+                data: {
+                    title: input.title,
+                    content: input.content,
+                    authorName: ctx.session.user.name || input.authorName,
+                    authorId: ctx.session.user.id,
+                    tags: input.tags,
+                }
+            })
+        })
+
+    // update ?
+    // delete ?
+});
