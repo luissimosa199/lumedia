a577775706927a9b1e2b2b00b21a9150f13bd235
Luis Simosa <simosa37@gmail.com>
2023-03-01 11:44:33 -0300

    react-hook-form initial implementation on newPost

--- src/components/newPost/newPost.tsx
+++ src/components/newPost/newPost.tsx
@@ -1,17 +1,82 @@
 import { FunctionComponent } from "react";
+import { useForm } from "react-hook-form";
+import { useSession } from "next-auth/react";
+import { api } from "~/utils/api";
 
 const NewPost: FunctionComponent = () => {
+  const { data: sessionData } = useSession();
+
+  const { refetch: refetchPosts } = api.post.getAll.useQuery();
+
+  const createPost = api.post.create.useMutation({
+    onSuccess: () => {
+      refetchPosts();
+    },
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    const processedData = {
+      title: data.title,
+      content: data.content,
+      tags: data.tags.split(", "),
+      authorName: sessionData?.user?.name || data.author,
+    };
+    createPost.mutate(processedData);
+    reset({
+        title: '',
+        content: '',
+        tags: '',
+    })
+  };
+
   return (
-    <form className="flex w-2/3 w-full flex-col gap-2 p-10">
+    <form
+      className="flex w-2/3 w-full flex-col gap-2 p-10"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <label htmlFor="title">Título</label>
-      <input id="title" name="title" type="text" className="rounded border" />
+      <input
+        id="title"
+        type="text"
+        className="rounded border"
+        {...register("title")}
+      />
       <label htmlFor="content">Contenido</label>
-      <textarea id="content" name="content" className="rounded border" />
+      <textarea
+        id="content"
+        className="rounded border"
+        {...register("content")}
+      />
       <label htmlFor="tags">Categorías</label>
-      <input id="tags" name="tags" type="text" className="rounded border" />
+      <input
+        id="tags"
+        type="text"
+        className="rounded border"
+        {...register("tags")}
+      />
+      <label htmlFor="author">Autor</label>
+      <input
+        id="author"
+        type="text"
+        className="rounded border"
+        {...register("author")}
+        disabled
+        value={sessionData?.user?.name || "Es necesario iniciar sesión"}
+      />
+
       <div className="flex justify-end gap-2">
         <button className="rounded border-2 px-4 py-2">Despejar</button>
-        <button className="rounded bg-darker-blue px-4 py-2 text-white">
+        <button
+          className="rounded bg-darker-blue px-4 py-2 text-white"
+          type="submit"
+        >
           Enviar
         </button>
       </div>
