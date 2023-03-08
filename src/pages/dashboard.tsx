009020c2994ed14bc0254d23ce067ff6fa4d656f
Luis Simosa <simosa37@gmail.com>
2023-03-08 13:19:43 -0300

    responsive design changes added

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,13 +6,13 @@ const Dashboard: FunctionComponent = () => {
   return (
     <div className="h-[90%]">
       <h1 className="mx-2 mb-2 text-gray-900 text-3xl font-bold">Panel de control de publicaciones</h1>
-      <div className="flex h-full">
+      <div className="flex flex-col-reverse h-full sm:flex-row">
 
-        <div className="flex flex-col gap-2 ml-2 w-1/3 p-2 max-h-[90vh] overflow-y-auto bg-grey rounded shadow-inner">
+        <div className="flex flex-col mb-2 sm:ml-2 mx-auto w-[95%] sm:w-1/3 p-2 max-h-[90vh] overflow-y-auto bg-grey rounded shadow-inner">
           <LatestPosts />
         </div>
 
-        <div className="w-2/3">
+        <div className="w-full sm:w-2/3">
           <NewPost/>
         </div>
       </div>
